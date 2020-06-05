11949690dbf5e5b9d43a8f1b8fb61926c620765d
Python Engineer <pythonengineerman@gmail.com>
2020-06-05 15:32:59 +0200

    changed stemmer, added nltk comments

--- nltk_utils.py
+++ nltk_utils.py
@@ -1,26 +1,36 @@
 import numpy as np
 import nltk
 # nltk.download('punkt')
-from nltk.stem.lancaster import LancasterStemmer
-stemmer = LancasterStemmer()
-
+from nltk.stem.porter import PorterStemmer
+stemmer = PorterStemmer()
 
 def tokenize(sentence):
+    """ split sentence into array of words/tokens """
     return nltk.word_tokenize(sentence)
 
 
-# stemming examples
-# print(stemmer.stem("maximum"), stemmer.stem("saying"), stemmer.stem("crying"))
 def stem(word):
+    """
+    stemming = find the root form of the word
+    examples:
+    words = ["organize", "organizes", "organizing"]
+    words = [stem(w) for w in words]
+    -> ["organ", "organ", "organ"]
+    """
     return stemmer.stem(word.lower())
 
 
 def bag_of_words(tokenized_sentence, words):
-    """return bag of words array: 
+    """
+    return bag of words array:
     1 for each known word that exists in the sentence, 0 otherwise
+    example:
+    sentence = ["hello", "how", "are", "you"]
+    words = ["hi", "hello", "I", "you", "bye", "thank", "cool"]
+    bog   = [  0 ,    1 ,    0 ,   1 ,    0 ,    0 ,      0]
     """
     # stem each word
-    sentence_words = [self.stem(word) for word in tokenized_sentence]
+    sentence_words = [stem(word) for word in tokenized_sentence]
     # initialize bag with 0 for each word
     bag = np.zeros(len(words), dtype=np.float32)
     for idx, w in enumerate(words):
